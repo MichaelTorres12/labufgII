1a3e0d30b74e9a38fd53aa0f3ef350b6063c5f8a
MichaelTorres12 <ia.michaeltorrescallejas@gmail.com>
2023-09-23 22:28:15 -0600

    michael02-botonFuncional

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SalariosComponent } from './salarios/salarios.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'salarios', component: SalariosComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
